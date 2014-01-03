cb857db349dd0df082336b4cb71a5bb96333dacc
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2014-01-03 19:54:09 +0000

    An example in answer to a question on StackOverflow.

---
+++ src/sandbox/objects/FitTextInRectangle.java
@@ -0,0 +1,59 @@
+/**
+ * Example written by Bruno Lowagie in answer to:
+ * http://stackoverflow.com/questions/20909450/correct-text-position-center-in-rectangle-itext
+ * 
+ * Doing some font math to vertically fit a piece of text inside a rectangle.
+ */
+package sandbox.objects;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class FitTextInRectangle {
+
+    public static void main(String[] args) throws IOException, DocumentException {
+
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(
+                "results/chunk_in_rectangle.pdf"));
+        document.open();
+        
+        // the direct content
+        PdfContentByte cb = writer.getDirectContent();
+        // the rectangle and the text we want to fit in the rectangle
+        Rectangle rect = new Rectangle(100, 150, 220, 200);
+        String text = "test";
+        // try to get max font size that fit in rectangle
+        BaseFont bf = BaseFont.createFont();
+        int textHeightInGlyphSpace = bf.getAscent(text) - bf.getDescent(text);
+        float fontSize = 1000f * rect.getHeight() / textHeightInGlyphSpace;
+        Phrase phrase = new Phrase("test", new Font(bf, fontSize));
+        ColumnText.showTextAligned(cb, Element.ALIGN_CENTER, phrase,
+                // center horizontally
+                (rect.getLeft() + rect.getRight()) / 2,
+                // shift baseline based on descent
+                rect.getBottom() - bf.getDescentPoint(text, fontSize),
+                0);
+
+        // draw the rect
+        cb.saveState();
+        cb.setColorStroke(BaseColor.BLUE);
+        cb.rectangle(rect.getLeft(), rect.getBottom(), rect.getWidth(), rect.getHeight());
+        cb.stroke();
+        cb.restoreState();
+        
+        document.close();
+    }
+}
