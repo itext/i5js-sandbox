c8795ad1d80164b2f5ad547ee08dcecb0a704982
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2014-01-30 12:35:20 +0000

    Another form merging example

---
+++ test/test/acroforms/MergeForms2.java
@@ -0,0 +1,17 @@
+/**
+ * Example written by Bruno Lowagie in answer to:
+ * http://stackoverflow.com/questions/20401125/overlapping-characters-in-text-field-itext-pdf
+ * 
+ * Sometimes you need to change the font of a field.
+ */
+package test.acroforms;
+
+import test.GenericTest;
+
+public class MergeForms2 extends GenericTest {
+
+	@Override
+	public void setup() {
+		setKlass("sandbox.acroforms.MergeForms2");
+	}
+}
