5bd9ccf5655a35abf223d21c10bf382d9b91b4fa
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2014-11-11 09:26:11 +0000

    Removed unused imports

--- src/main/java/sandbox/images/TiledImage.java
+++ src/main/java/sandbox/images/TiledImage.java
@@ -8,13 +8,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import sandbox.WrapToTest;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
