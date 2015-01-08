037212e9b2a44dcce1a7222fdf056e0e29f4af2c
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-01-08 18:04:23 +0000

    Removing unused imports

--- src/main/java/sandbox/objects/ListAlignment.java
+++ src/main/java/sandbox/objects/ListAlignment.java
@@ -10,15 +10,11 @@ import java.io.IOException;
 
 import sandbox.WrapToTest;
 
-import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.List;
 import com.itextpdf.text.ListItem;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 @WrapToTest
