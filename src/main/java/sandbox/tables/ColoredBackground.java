d38731bcea444e494d42c0cda7340969cbb4a72e
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-01-10 15:40:17 +0000

    Extra examples in answer to SO questions

---
+++ src/main/java/sandbox/tables/ColoredBackground.java
@@ -0,0 +1,52 @@
+/**
+ * Example written by Bruno Lowagie and Nishanthi Grashia in answer to the following question:
+ * http://stackoverflow.com/questions/27871574/appears-space-between-cells-without-border-itextpdf
+ */
+package sandbox.tables;
+
+import com.itextpdf.text.BaseColor;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class ColoredBackground {
+    public static final String DEST = "results/tables/colored_background.pdf";
+
+    public static void main(String[] args) throws IOException,
+            DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ColoredBackground().createPdf(DEST);
+    }
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(dest));
+        document.open();
+        PdfPTable table;
+        PdfPCell cell;
+        Font font = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.BOLD, BaseColor.WHITE);
+        table = new PdfPTable(16);
+        for(int aw = 0; aw < 16; aw++){
+            cell = new PdfPCell(new Phrase("hi", font));
+            cell.setBackgroundColor(BaseColor.BLUE);
+            cell.setBorder(Rectangle.NO_BORDER);
+            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.addCell(cell);
+        }
+        document.add(table);
+        document.close();
+    }
+
+}
---
+++ src/main/java/sandbox/tables/IndentationInCell.java
@@ -0,0 +1,55 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/27873550/how-to-indent-text-inside-a-pdfpcell
+ */
+package sandbox.tables;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class IndentationInCell {
+    public static final String DEST = "results/tables/indentation_in_cell.pdf";
+
+    public static void main(String[] args) throws IOException,
+            DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new IndentationInCell().createPdf(DEST);
+    }
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(dest));
+        document.open();
+        PdfPTable table = new PdfPTable(1);
+        PdfPCell cell;
+        cell = new PdfPCell(new Phrase("TO:\n\n   name"));
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase("TO:\n\n\u00a0\u00a0\u00a0name"));
+        table.addCell(cell);
+        cell = new PdfPCell();
+        cell.addElement(new Paragraph("TO:"));
+        Paragraph p = new Paragraph("name");
+        p.setIndentationLeft(10);
+        cell.addElement(p);
+        table.addCell(cell);
+        cell = new PdfPCell();
+        cell.addElement(new Paragraph("TO:"));
+        p = new Paragraph("name");
+        p.setAlignment(Element.ALIGN_RIGHT);
+        cell.addElement(p);
+        table.addCell(cell);
+        document.add(table);
+        document.close();
+    }
+
+}
