f059f63c477d1b1491d814b5d60128f7605b9b86
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-01-12 18:54:14 +0000

    Remove unused imports Adding an extra example in answer to a question on SO

--- src/main/java/sandbox/parse/RemoveRedactedContent.java
+++ src/main/java/sandbox/parse/RemoveRedactedContent.java
@@ -5,36 +5,29 @@
 
 package sandbox.parse;
 
-import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
-import com.itextpdf.text.pdf.pdfcleanup.PdfCleanUpLocation;
 import com.itextpdf.text.pdf.pdfcleanup.PdfCleanUpProcessor;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class RemoveContentInRectangle {
+public class RemoveRedactedContent {
 
-    public static final String SRC = "resources/pdfs/page229.pdf";
-    public static final String DEST = "results/parse/page229_removed_content.pdf";
+    public static final String SRC = "resources/pdfs/page229_redacted.pdf";
+    public static final String DEST = "results/parse/page229_apply_redacted.pdf";
     
     public static void main(String[] args) throws IOException, DocumentException {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
-        new RemoveContentInRectangle().manipulatePdf(SRC, DEST);
+        new RemoveRedactedContent().manipulatePdf(SRC, DEST);
     }
     
     public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
         PdfReader reader = new PdfReader(src);
         PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
-        List<PdfCleanUpLocation> cleanUpLocations = new ArrayList<PdfCleanUpLocation>();
-        cleanUpLocations.add(new PdfCleanUpLocation(1, new Rectangle(97f, 405f, 480f, 445f), BaseColor.GRAY));
-        PdfCleanUpProcessor cleaner = new PdfCleanUpProcessor(cleanUpLocations, stamper);
+        PdfCleanUpProcessor cleaner = new PdfCleanUpProcessor(stamper);
         cleaner.cleanUp();
         stamper.close();
         reader.close();
