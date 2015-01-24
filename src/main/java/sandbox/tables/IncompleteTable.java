c6ffcd93254e7a9999784a7df6fcf4c11c5607ef
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-01-24 11:15:04 +0000

    New example to reproduce a problem that was introduce by the latest fix. Changing the init() method of the PdfPTable object.

---
+++ src/main/java/sandbox/tables/IncompleteTable.java
@@ -0,0 +1,49 @@
+/**
+ * Example written by Bruno Lowagie
+ */
+package sandbox.tables;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class IncompleteTable {
+    public static final String DEST = "results/tables/incomplete_table.pdf";
+
+    public static void main(String[] args) throws IOException,
+            DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new IncompleteTable().createPdf(DEST);
+    }
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document(PageSize.LETTER);
+        PdfWriter.getInstance(document, new FileOutputStream(dest));
+
+        document.open();
+        PdfPTable table = new PdfPTable(5);
+        table.setHeaderRows(1);
+        table.setSplitRows(false);
+        table.setComplete(false);
+
+        for (int i = 0; i < 5; i++) {table.addCell("Header " + i);}
+
+        for (int i = 0; i < 500; i++) {
+            if (i%5 == 0) {
+                document.add(table);
+            }
+            table.addCell("Test " + i);
+        }
+
+        table.setComplete(true);
+        document.add(table);
+        document.close();
+    }
+
+}
