d5a818ae8fdbf8a5eb08b2a7db24ea3ca1fa24b7
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-01-27 08:57:26 +0000

    Extra example in answer to SO question

---
+++ src/main/java/sandbox/acroforms/FlattenSignature.java
@@ -0,0 +1,33 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/28163159/about-visible-digital-signatures
+ */
+package sandbox.acroforms;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+public class FlattenSignature {
+
+    public static final String SRC = "resources/pdfs/input_signed.pdf";
+    public static final String DEST = "results/acroforms/output_no_longer_signed.pdf";
+    
+
+    public static void main(String[] args) throws DocumentException, IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new FlattenSignature().manipulatePdf(SRC, DEST);
+    }
+
+    public void manipulatePdf(String src, String dest) throws DocumentException, IOException {
+        PdfReader reader = new PdfReader(src);
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        stamper.setFormFlattening(true);
+        stamper.close();
+    }
+}
