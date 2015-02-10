e2c50456d2d357a6f899f52ba98f5c9b29b47cfc
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-02-10 15:00:24 +0000

    New example in answer to a StackOverflow question.

---
+++ src/main/java/sandbox/objects/ChapterAndTitle.java
@@ -0,0 +1,43 @@
+/**
+ * Example written by Bruno Lowagie in answer to:
+ * http://stackoverflow.com/questions/28431148/itext-chapter-font-overrides-paragraph-font
+ */
+package sandbox.objects;
+
+import com.itextpdf.text.Chapter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.DottedLineSeparator;
+
+public class ChapterAndTitle {
+    public static final String DEST = "results/objects/chapter_title.pdf";
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ChapterAndTitle().createPdf(DEST);
+    }
+    
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(dest));
+        document.open();
+        Font chapterFont = FontFactory.getFont(FontFactory.HELVETICA, 16, Font.BOLDITALIC);
+        Font paragraphFont = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.NORMAL);
+        Chunk chunk = new Chunk("This is the title", chapterFont);
+        Chapter chapter = new Chapter(new Paragraph(chunk), 1);
+        chapter.setNumberDepth(0);
+        chapter.add(new Paragraph("This is the paragraph", paragraphFont));
+        document.add(chapter);
+        document.close();
+    }
+}
