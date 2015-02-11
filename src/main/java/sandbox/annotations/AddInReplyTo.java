8d0a1b2af066e1e544b1eefa973ae0d81d03e9f2
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-02-11 12:08:40 +0000

    Extra example in answer to a question on StackOverflow.

---
+++ src/main/java/sandbox/annotations/AddInReplyTo.java
@@ -0,0 +1,50 @@
+package sandbox.annotations;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import sandbox.WrapToTest;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfAnnotation;
+import com.itextpdf.text.pdf.PdfArray;
+import com.itextpdf.text.pdf.PdfDestination;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfWriter;
+
+@WrapToTest
+public class AddInReplyTo {
+
+    public static final String SRC = "resources/pdfs/hello_sticky_note.pdf";
+    public static final String DEST = "results/annotations/hello_in_reply_to.pdf";
+
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new AddInReplyTo().manipulatePdf(SRC, DEST);
+    }
+    public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(src);
+        PdfDictionary page = reader.getPageN(1);
+        PdfArray annots = page.getAsArray(PdfName.ANNOTS);
+        PdfDictionary sticky = annots.getAsDict(0);
+        PdfArray stickyRect = sticky.getAsArray(PdfName.RECT);
+        PdfDictionary popup = annots.getAsDict(1);
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        PdfWriter writer = stamper.getWriter();
+        Rectangle stickyRectangle = new Rectangle(
+            stickyRect.getAsNumber(0).floatValue(), stickyRect.getAsNumber(1).floatValue(),
+            stickyRect.getAsNumber(2).floatValue(), stickyRect.getAsNumber(3).floatValue()
+        );
+        PdfAnnotation replySticky = PdfAnnotation.createText(
+                writer, stickyRectangle, "Reply", "Hello PDF", true, "Comment");
+        replySticky.put(PdfName.IRT, annots.getAsIndirectObject(0));
+        stamper.addAnnotation(replySticky, 1);
+        stamper.close();
+    }
+}
