26e4bb8c5e0a8d61f4c82c38fb37021e5534f5d3
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-02-18 17:30:51 +0000

    New example in answer to a question on StackOverflow.

---
+++ src/main/java/sandbox/acroforms/FillWithUnderline.java
@@ -0,0 +1,48 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/28579382/underline-portion-of-text-using-itextsharp
+ */
+package sandbox.acroforms;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.AcroFields.FieldPosition;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.tool.xml.ElementList;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+public class FillWithUnderline {
+
+    public static final String SRC = "resources/pdfs/form.pdf";
+    public static final String DEST = "results/acroforms/field_underlined.pdf";
+    
+
+    public static void main(String[] args) throws DocumentException, IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new FillWithUnderline().manipulatePdf(SRC, DEST);
+    }
+
+    public void manipulatePdf(String src, String dest) throws DocumentException, IOException {
+        PdfReader reader = new PdfReader(src);
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        stamper.setFormFlattening(true);
+        AcroFields form = stamper.getAcroFields();
+        FieldPosition pos = form.getFieldPositions("Name").get(0);
+        ColumnText ct = new ColumnText(stamper.getOverContent(pos.page));
+        ct.setSimpleColumn(pos.position);
+        ElementList elements = XMLWorkerHelper.parseToElementList("<div>Bruno <u>Lowagie</u></div>", null);
+        for (Element element : elements) {
+            ct.addElement(element);
+        }
+        ct.go();
+        stamper.close();
+    }
+}
