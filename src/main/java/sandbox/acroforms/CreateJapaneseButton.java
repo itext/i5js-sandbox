f202e3c9baeea348df2c4d455323d9454ea5e191
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-03-13 16:53:30 +0100

    NetBeans problem

--- src/main/java/sandbox/acroforms/CreateJapaneseButton.java
+++ src/main/java/sandbox/acroforms/CreateJapaneseButton.java
@@ -1,6 +1,6 @@
 /**
  * Example written by Bruno Lowagie in answer to:
- * http://stackoverflow.com/questions/21028286/itext-editable-texfield-is-not-clickable
+ * http://stackoverflow.com/questions/29016333/pushbuttonfield-with-double-byte-character
  */
 package sandbox.acroforms;
 
@@ -10,64 +10,33 @@ import java.io.IOException;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.ExceptionConverter;
-import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PdfContentByte;
-import com.itextpdf.text.pdf.PdfFormField;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPCellEvent;
-import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.pdf.TextField;
+import com.itextpdf.text.pdf.PushbuttonField;
 
-public class CreateFormInTable {
+public class CreateJapaneseButton {
     
-    class MyCellField implements PdfPCellEvent {
-        protected String fieldname;
-        public MyCellField(String fieldname) {
-            this.fieldname = fieldname;
-        }
-        public void cellLayout(PdfPCell cell, Rectangle rectangle, PdfContentByte[] canvases) {
-            final PdfWriter writer = canvases[0].getPdfWriter();
-            final TextField textField = new TextField(writer, rectangle, fieldname);
-            try {
-                final PdfFormField field = textField.getTextField();
-                writer.addAnnotation(field);
-            } catch (final IOException ioe) {
-                throw new ExceptionConverter(ioe);
-            } catch (final DocumentException de) {
-                throw new ExceptionConverter(de);
-            }
-        }
-    }
-    
-    public static final String DEST = "results/acroforms/form_in_table.pdf";
+    public static final String DEST = "results/acroforms/japanese_button.pdf";
+    public static final String JAPANESE = "\u3042\u304d\u3089";
+    public static final String FONT = "resources/fonts/FreeSans.ttf";
 
     public static void main(String[] args) throws IOException,
             DocumentException {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
-        new CreateFormInTable().createPdf(DEST);
+        new CreateJapaneseButton().createPdf(DEST);
     }
     
     public void createPdf(String dest) throws IOException, DocumentException {
         Document document = new Document();
-        PdfWriter.getInstance(document, new FileOutputStream(dest));
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
         document.open();
-        PdfPTable table = new PdfPTable(2);
-        PdfPCell cell;
-        cell = new PdfPCell(new Phrase("Name:"));
-        table.addCell(cell);
-        cell = new PdfPCell();
-        cell.setCellEvent(new MyCellField("name"));
-        table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Address"));
-        table.addCell(cell);
-        cell = new PdfPCell();
-        cell.setCellEvent(new MyCellField("address"));
-        table.addCell(cell);
-        document.add(table);
+        PushbuttonField button = new PushbuttonField(writer, new Rectangle(36, 780, 144, 806), "japanese");
+        BaseFont bf = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        button.setFont(bf);
+        button.setText(JAPANESE);
+        writer.addAnnotation(button.getField());
         document.close();
     }
 
