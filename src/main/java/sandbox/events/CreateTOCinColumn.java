35979bc9b59bb83d0eb08c42e17bfde492b9f8b7
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-03-17 14:02:05 +0100

    Two examples in answer to questions in StackOverflow.

---
+++ src/main/java/sandbox/events/CreateTOCinColumn.java
@@ -0,0 +1,116 @@
+/**
+ * Example written by Bruno Lowagie in answer to:
+ * http://stackoverflow.com/questions/29092738/itext-chapter-title-and-columntext
+ */
+package sandbox.events;
+
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfAction;
+import com.itextpdf.text.pdf.PdfDestination;
+import com.itextpdf.text.pdf.PdfOutline;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateTOCinColumn {
+    
+    public static final String DEST = "results/events/columns_with_toc.pdf";
+    public static final Rectangle[] COLUMNS = {
+        new Rectangle(36, 36, 192, 806),
+        new Rectangle(204, 36, 348, 806),
+        new Rectangle(360, 36, 504, 806)
+    };
+    
+    public class TOCEntry {
+        protected PdfAction action;
+        protected String title;
+    }
+    
+    public class TOCCreation extends PdfPageEventHelper {
+
+        protected PdfOutline root;
+        protected List<TOCEntry> toc = new ArrayList<TOCEntry>();
+        
+        public TOCCreation() {
+        }
+        
+        public void setRoot(PdfOutline root) {
+            this.root = root;
+        }
+        
+        public List<TOCEntry> getToc() {
+            return toc;
+        }
+        
+        @Override
+        public void onGenericTag(PdfWriter writer, Document document, Rectangle rect, String text) {
+            PdfDestination dest = new PdfDestination(PdfDestination.XYZ, rect.getLeft(), rect.getTop(), 0);
+            new PdfOutline(root, dest, text);
+            TOCEntry entry = new TOCEntry();
+            entry.action = PdfAction.gotoLocalPage(writer.getPageNumber(), dest, writer);
+            entry.title = text;
+            toc.add(entry);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new CreateTOCinColumn().createPdf(DEST);
+    }
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+        TOCCreation event = new TOCCreation();
+        writer.setPageEvent(event);
+        document.open();
+        event.setRoot(writer.getRootOutline());
+        ColumnText ct = new ColumnText(writer.getDirectContent());
+        int start;
+        int end;
+        for (int i = 0; i <= 20; ) {
+            start = (i * 10) + 1;
+            i++;
+            end = i * 10;
+            String title = String.format("Numbers from %s to %s", start, end);
+            Chunk c = new Chunk(title);
+            c.setGenericTag(title);
+            ct.addElement(c);
+            ct.addElement(createTable(start, end));
+        }
+        int column = 0;
+        do {
+            if (column == 3) {
+                document.newPage();
+                column = 0;
+            }
+            ct.setSimpleColumn(COLUMNS[column++]);
+        } while (ColumnText.hasMoreText(ct.go()));
+        document.newPage();
+        for (TOCEntry entry : event.getToc()) {
+            Chunk c = new Chunk(entry.title);
+            c.setAction(entry.action);
+            document.add(new Paragraph(c));
+        }
+        document.close();
+    }
+    
+    public PdfPTable createTable(int start, int end) throws IOException {
+        PdfPTable table = new PdfPTable(2);
+        for (int i = start; i <= end; i++) {
+            table.addCell(String.valueOf(i));
+            table.addCell("Test");
+        }
+        return table;
+    }
+}
---
+++ src/main/java/sandbox/images/MakeJpgMask.java
@@ -0,0 +1,52 @@
+/**
+ * This code sample was written by Bruno Lowagie in answer to this question:
+ * http://stackoverflow.com/questions/29096314/how-to-make-an-image-a-qualified-mask-candidate-in-itext
+ */
+package sandbox.images;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import javax.imageio.ImageIO;
+
+public class MakeJpgMask {
+    public static final String IMAGE = "resources/images/javaone2013.jpg";
+    public static final String MASK = "resources/images/berlin2013.jpg";
+    public static final String DEST = "results/images/jpg_mask.pdf";
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new MakeJpgMask().createPdf(DEST);
+    }
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document(PageSize.A4.rotate());
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+        document.open();
+        Image image = Image.getInstance(IMAGE);
+        Image mask = makeBlackAndWhitePng(MASK);
+        mask.makeMask();
+        image.setImageMask(mask);
+        image.scaleAbsolute(PageSize.A4.rotate());
+        image.setAbsolutePosition(0, 0);
+        document.add(image);
+        document.close();
+    }
+    
+    public static Image makeBlackAndWhitePng(String image) throws IOException, DocumentException {
+        BufferedImage bi = ImageIO.read(new File(image));
+        BufferedImage newBi = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_USHORT_GRAY);
+        newBi.getGraphics().drawImage(bi, 0, 0, null);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(newBi, "png", baos);
+        return Image.getInstance(baos.toByteArray());
+    }
+}
