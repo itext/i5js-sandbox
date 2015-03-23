21264d088a3d42320cc9c2f97e1fe6f602dde3d4
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-03-23 12:28:01 +0100

    new examples in answer to SO questions

---
+++ src/main/java/sandbox/events/PageOrientations.java
@@ -0,0 +1,64 @@
+/**
+ * This example was written by Bruno Lowagie.
+ */
+package sandbox.events;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfNumber;
+import com.itextpdf.text.pdf.PdfPage;
+import com.itextpdf.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class PageOrientations {
+    public static final String DEST = "results/events/orientations.pdf";
+    
+    public class Rotate extends PdfPageEventHelper {
+        
+        protected PdfNumber orientation = PdfPage.PORTRAIT;
+        
+        public void setOrientation(PdfNumber orientation) {
+            this.orientation = orientation;
+        }
+        
+        public void onStartPage(PdfWriter writer, Document document) {
+            writer.addPageDictEntry(PdfName.ROTATE, orientation);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new PageOrientations().createPdf(DEST);
+    }
+    
+    public void createPdf(String filename) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(filename));
+        Rotate event = new Rotate();
+        writer.setPageEvent(event);
+        // step 3
+        document.open();
+        // step 4
+        document.add(new Paragraph("A simple page in portrait orientation"));
+        event.setOrientation(PdfPage.LANDSCAPE);
+        document.newPage();
+        document.add(new Paragraph("A simple page in landscape orientation"));;
+        event.setOrientation(PdfPage.INVERTEDPORTRAIT);
+        document.newPage();
+        document.add(new Paragraph("A simple page in inverted portrait orientation"));;
+        event.setOrientation(PdfPage.SEASCAPE);
+        document.newPage();
+        document.add(new Paragraph("A simple page in seascape orientation"));
+        // step 5
+        document.close();
+    }
+}
---
+++ src/main/java/sandbox/events/ScaleDown.java
@@ -0,0 +1,94 @@
+/**
+ * Example written by Bruno Lowagie in answer to
+ * http://stackoverflow.com/questions/29152313/fix-the-orientation-of-a-pdf-in-order-to-scale-it
+ */
+package sandbox.events;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfArray;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfRectangle;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * This solution is suboptimal as it throws away all interactivity.
+ * If you want to keep the interactive elements (annotations, form fields,...),
+ * you need to do much more work. If you don't need to scale down, but instead
+ * only have to scale up, you should try the example that was written in
+ * answer to http://stackoverflow.com/questions/21871027/rotating-in-itextsharp-while-preserving-comment-location-orientation
+ */
+public class ScaleDown {
+    
+    public static final String SRC = "resources/pdfs/orientations.pdf";
+    public static final String DEST = "results/events/scaled_down.pdf";
+        
+    public class ScaleEvent extends PdfPageEventHelper {
+        
+        protected float scale = 1;
+        protected PdfDictionary pageDict;
+        
+        public ScaleEvent(float scale) {
+            this.scale = scale;
+        }
+        
+        public void setPageDict(PdfDictionary pageDict) {
+            this.pageDict = pageDict;
+        }
+        
+        @Override
+        public void onStartPage(PdfWriter writer, Document document) {
+            writer.addPageDictEntry(PdfName.ROTATE, pageDict.getAsNumber(PdfName.ROTATE));
+            writer.addPageDictEntry(PdfName.MEDIABOX, scaleDown(pageDict.getAsArray(PdfName.MEDIABOX), scale));
+            writer.addPageDictEntry(PdfName.CROPBOX, scaleDown(pageDict.getAsArray(PdfName.CROPBOX), scale));
+        }
+    }
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ScaleDown().manipulatePdf(SRC, DEST);
+    }
+    
+    public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(src);
+        float scale = 0.5f;
+        ScaleEvent event = new ScaleEvent(scale);
+        event.setPageDict(reader.getPageN(1));
+        
+        int n = reader.getNumberOfPages();
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+        writer.setPageEvent(event);
+        document.open();
+        Image page;
+        for (int p = 1; p <= n; p++) {
+            page = Image.getInstance(writer.getImportedPage(reader, p));
+            page.setAbsolutePosition(0, 0);
+            page.scalePercent(scale * 100);
+            document.add(page);
+            if (p < n) {
+                event.setPageDict(reader.getPageN(p + 1));
+            }
+            document.newPage();
+        }
+        document.close();
+    }
+    
+    public PdfArray scaleDown(PdfArray original, float scale) {
+        if (original == null)
+            return null;
+        float width = original.getAsNumber(2).floatValue()
+                - original.getAsNumber(0).floatValue();
+        float height = original.getAsNumber(3).floatValue()
+                - original.getAsNumber(1).floatValue();
+        return new PdfRectangle(width * scale, height * scale);
+    }
+}
