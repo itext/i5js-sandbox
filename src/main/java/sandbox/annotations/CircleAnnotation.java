cb4ba88faf9732e8f3a4ce92070057a6f141d527
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-03-26 13:52:29 +0100

    ZUGFeRD: what do we need to meet the BASIC level. Two new examples in answer to questions on SO.

---
+++ src/main/java/sandbox/annotations/CircleAnnotation.java
@@ -0,0 +1,46 @@
+/**
+ * This example was written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/29275194/change-pdf-fillcolor-annotation-property-using-itextsharp-c-sharp
+ */
+package sandbox.annotations;
+
+import com.itextpdf.text.BaseColor;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfAnnotation;
+import com.itextpdf.text.pdf.PdfArray;
+import com.itextpdf.text.pdf.PdfBorderArray;
+import com.itextpdf.text.pdf.PdfDashPattern;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+public class CircleAnnotation {
+
+    public static final String SRC = "resources/pdfs/hello.pdf";
+    public static final String DEST = "results/annotations/hello_circle.pdf";
+
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new CircleAnnotation().manipulatePdf(SRC, DEST);
+    }
+    public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(src);
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        Rectangle rect = new Rectangle(150, 770, 200, 820);
+        PdfAnnotation annotation = PdfAnnotation.createSquareCircle(
+            stamper.getWriter(), rect, "Circle", false);
+        annotation.setTitle("Circle");
+        annotation.setColor(BaseColor.BLUE);
+        annotation.setFlags(PdfAnnotation.FLAGS_PRINT);
+        annotation.setBorder(new PdfBorderArray(0, 0, 2, new PdfDashPattern()));
+        annotation.put(PdfName.IC, new PdfArray(new int[]{1, 0, 0}));
+        stamper.addAnnotation(annotation, 1);
+        stamper.close();
+    }
+}
---
+++ src/main/java/sandbox/objects/ListInColumn.java
@@ -0,0 +1,55 @@
+/**
+ * Example written by Bruno Lowagie in answer to:
+ * http://stackoverflow.com/questions/29277611/itextsharp-continuing-ordered-list-on-second-page-with-a-number-other-than-1
+ */
+package sandbox.objects;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import sandbox.WrapToTest;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.List;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+@WrapToTest
+public class ListInColumn {
+
+    public static final String SRC = "resources/pdfs/pages.pdf";
+    public static final String DEST = "results/objects/list_in_column.pdf";
+    
+    public static void main(String[] args) throws IOException,
+            DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ListInColumn().manipulatePdf(SRC, DEST);
+    }
+    
+    public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(src);
+        reader.selectPages("1-2");
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        List list = new List(List.ORDERED);
+        for (int i = 0; i < 10; i++) {
+            list.add("This is a list item. It will be repeated a number of times. "
+                    + "This is done only for test purposes. "
+                    + "I want a list item that is distributed over several lines.");
+        }
+        ColumnText ct = new ColumnText(stamper.getOverContent(1));
+        ct.addElement(list);
+        Rectangle rect = new Rectangle(250, 400, 500, 806);
+        ct.setSimpleColumn(rect);
+        int status = ct.go();
+        if (ColumnText.hasMoreText(status)) {
+            ct.setCanvas(stamper.getOverContent(2));
+            ct.setSimpleColumn(rect);
+            ct.go();
+        }
+        stamper.close();
+    }
+}
