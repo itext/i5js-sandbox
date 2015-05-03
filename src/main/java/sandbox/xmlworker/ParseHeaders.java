76884b9395efbd9504e783c01d9d05e8a3d2e731
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-05-03 14:32:54 +0200

    Adding an extra example in answer to a question on SO.

---
+++ src/main/java/sandbox/xmlworker/ParseHeaders.java
@@ -0,0 +1,50 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/29290668/set-line-spacing-when-using-xmlworker-to-parse-html-to-pdf-itextsharp-c-sharp
+ */
+package sandbox.xmlworker;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author iText
+ */
+public class ParseHeaders {
+    public static final String DEST = "results/xmlworker/headers.pdf";
+    public static final String HTML = "resources/xml/headers.html";
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ParseHeaders().createPdf(DEST);
+    }
+        
+    /**
+     * Creates a PDF with the words "Hello World"
+     * @param file
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void createPdf(String file) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+        writer.setTagged();
+        // step 3
+        document.open();
+        // step 4
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                new FileInputStream(HTML));
+        // step 5
+        document.close();
+    }
+}
