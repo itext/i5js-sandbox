5e76ac801e54a1233147e0c8f958c5bf4b4aaa74
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-05-13 18:00:14 +0200

    Three examples converting Arabic HTML to PDF

---
+++ src/main/java/sandbox/xmlworker/ParseHtml7.java
@@ -0,0 +1,90 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/30214147/arabic-characters-from-html-content-to-pdf-using-itext
+ */
+package sandbox.xmlworker;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.ElementList;
+import com.itextpdf.tool.xml.XMLWorker;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
+import com.itextpdf.tool.xml.css.StyleAttrCSSResolver;
+import com.itextpdf.tool.xml.html.CssAppliers;
+import com.itextpdf.tool.xml.html.CssAppliersImpl;
+import com.itextpdf.tool.xml.html.Tags;
+import com.itextpdf.tool.xml.parser.XMLParser;
+import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
+import com.itextpdf.tool.xml.pipeline.css.CssResolverPipeline;
+import com.itextpdf.tool.xml.pipeline.end.ElementHandlerPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author iText
+ */
+public class ParseHtml7 {
+    public static final String DEST = "results/xmlworker/arabic.pdf";
+    public static final String HTML = "resources/xml/arabic.html";
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ParseHtml7().createPdf(DEST);
+    }
+        
+    /**
+     * Creates a PDF with the words "Hello World"
+     * @param file
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void createPdf(String file) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+        // step 3
+        document.open();
+        // step 4
+        // Styles
+        CSSResolver cssResolver = new StyleAttrCSSResolver();
+        XMLWorkerFontProvider fontProvider = new XMLWorkerFontProvider(XMLWorkerFontProvider.DONTLOOKFORFONTS);
+        fontProvider.register("resources/fonts/NotoNaskhArabic-Regular.ttf");
+        CssAppliers cssAppliers = new CssAppliersImpl(fontProvider);
+        // HTML
+        HtmlPipelineContext htmlContext = new HtmlPipelineContext(cssAppliers);
+        htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
+        // Pipelines
+        ElementList elements = new ElementList();
+        ElementHandlerPipeline pdf = new ElementHandlerPipeline(elements, null);
+        HtmlPipeline html = new HtmlPipeline(htmlContext, pdf);
+        CssResolverPipeline css = new CssResolverPipeline(cssResolver, html);
+        
+        // XML Worker
+        XMLWorker worker = new XMLWorker(css, true);
+        XMLParser p = new XMLParser(worker);
+        p.parse(new FileInputStream(HTML), Charset.forName("UTF-8"));
+        
+        PdfPTable table = new PdfPTable(1);
+        PdfPCell cell = new PdfPCell();
+        cell.setRunDirection(PdfWriter.RUN_DIRECTION_RTL);
+        for (Element e : elements) {
+            cell.addElement(e);
+        }
+        table.addCell(cell);
+        document.add(table);
+        // step 5
+        document.close();
+    }
+}
---
+++ src/main/java/sandbox/xmlworker/ParseHtml9.java
@@ -0,0 +1,76 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/30214147/arabic-characters-from-html-content-to-pdf-using-itext
+ */
+package sandbox.xmlworker;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorker;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
+import com.itextpdf.tool.xml.css.StyleAttrCSSResolver;
+import com.itextpdf.tool.xml.html.CssAppliers;
+import com.itextpdf.tool.xml.html.CssAppliersImpl;
+import com.itextpdf.tool.xml.html.Tags;
+import com.itextpdf.tool.xml.parser.XMLParser;
+import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
+import com.itextpdf.tool.xml.pipeline.css.CssResolverPipeline;
+import com.itextpdf.tool.xml.pipeline.end.PdfWriterPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author iText
+ */
+public class ParseHtml9 {
+    public static final String DEST = "results/xmlworker/arabic3.pdf";
+    public static final String HTML = "resources/xml/arabic3.html";
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ParseHtml9().createPdf(DEST);
+    }
+        
+    /**
+     * Creates a PDF with the words "Hello World"
+     * @param file
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void createPdf(String file) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+        // step 3
+        document.open();
+        // step 4
+        // Styles
+        CSSResolver cssResolver = new StyleAttrCSSResolver();
+        XMLWorkerFontProvider fontProvider = new XMLWorkerFontProvider(XMLWorkerFontProvider.DONTLOOKFORFONTS);
+        fontProvider.register("resources/fonts/NotoNaskhArabic-Regular.ttf");
+        CssAppliers cssAppliers = new CssAppliersImpl(fontProvider);
+        HtmlPipelineContext htmlContext = new HtmlPipelineContext(cssAppliers);
+        htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
+        
+        // Pipelines
+        PdfWriterPipeline pdf = new PdfWriterPipeline(document, writer);
+        HtmlPipeline html = new HtmlPipeline(htmlContext, pdf);
+        CssResolverPipeline css = new CssResolverPipeline(cssResolver, html);
+        
+        // XML Worker
+        XMLWorker worker = new XMLWorker(css, true);
+        XMLParser p = new XMLParser(worker);
+        p.parse(new FileInputStream(HTML), Charset.forName("UTF-8"));;
+        // step 5
+        document.close();
+    }
+}
