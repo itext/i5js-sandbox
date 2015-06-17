39d8319113653d77b9d04a4c22c410f31949c6b2
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-06-17 15:28:00 +0200

    XML Worker example using Cyrillic text

---
+++ src/main/java/sandbox/xmlworker/ParseHtml11.java
@@ -0,0 +1,56 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/30889217/html-to-pdf-convert-cyrillic-characters-not-displayed-properly
+ */
+package sandbox.xmlworker;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * @author iText
+ */
+public class ParseHtml11 {
+    public static final String DEST = "results/xmlworker/cyrillic.pdf";
+    public static final String HTML = "resources/xml/cyrillic.html";
+    
+    public static final String FONT = "resources/fonts/FreeSans.ttf";
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ParseHtml11().createPdf(DEST);
+    }
+        
+    /**
+     * Creates a PDF with the words "Hello World"
+     * @param file
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void createPdf(String file) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+        // step 3
+        document.open();
+        // step 4
+        XMLWorkerFontProvider fontImp = new XMLWorkerFontProvider(XMLWorkerFontProvider.DONTLOOKFORFONTS);
+        fontImp.register(FONT);
+        FontFactory.setFontImp(fontImp);
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                new FileInputStream(HTML), null, Charset.forName("UTF-8"), fontImp);
+        // step 5
+        document.close();
+    }
+}
