d59c9078b8d277f82bd5b0eadfcef26bd10d959f
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-06-24 10:58:00 +0200

    Extra example in answer to a StackOverflow question.
    Parsing a table in HTML with styles defined in the HTML header.

---
+++ src/main/java/sandbox/xmlworker/ParseHtmlTable11.java
@@ -0,0 +1,45 @@
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/31020576/how-to-customize-fontstyles-while-creating-pdf-from-html-using-itext
+ */
+package sandbox.xmlworker;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ParseHtmlTable11 {
+    public static final String DEST = "results/xmlworker/html_table_11.pdf";
+    public static final String HTML = "resources/xml/table11.html";
+    
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ParseHtmlTable11().createPdf(DEST);
+    }
+        
+    /**
+     * Creates a PDF with the words "Hello World"
+     * @param file
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void createPdf(String file) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+        // step 3
+        document.open();
+        // step 4
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                new FileInputStream(HTML));
+        // step 5
+        document.close();
+    }
+}
