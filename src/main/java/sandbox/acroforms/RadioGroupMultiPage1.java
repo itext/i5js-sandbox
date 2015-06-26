f38b2c03a000d6f1f0209832148543073a9c8212
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-06-26 16:13:42 +0200

    Extra examples in answer to a question on StackOverflow.
    Radio field with widgets on different pages:
    Example showing how it was before (taken from iText in Action 2E)
    Example demonstrating how it can be done today.
    
    See #DEC-1455 for more info.

---
+++ src/main/java/sandbox/acroforms/RadioGroupMultiPage1.java
@@ -0,0 +1,71 @@
+/*
+ * This example was written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/30895930/issue-with-itext-radiocheckfield-when-displayed-on-multiple-pages
+ */
+package sandbox.acroforms;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.GrayColor;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfFormField;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.RadioCheckField;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class RadioGroupMultiPage1 {
+    
+    public static final String DEST = "results/acroforms/multipage_radiobutton1.pdf";
+    /** Possible values of a Choice field. */
+    public static final String[] LANGUAGES = { "English", "German", "French", "Spanish", "Dutch" };
+ 
+    public static void main(String[] args) throws DocumentException, IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new RadioGroupMultiPage1().createPdf(DEST);
+    }
+    public void createPdf(String dest) throws IOException, DocumentException {
+    	// step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer =
+            PdfWriter.getInstance(document, new FileOutputStream(dest));
+        // step 3
+        document.open();
+        // step 4
+        PdfContentByte cb = writer.getDirectContent();
+        BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.NOT_EMBEDDED);
+        // create a radio field spanning different pages
+        PdfFormField radiogroup = PdfFormField.createRadioButton(writer, true);
+        radiogroup.setFieldName("language");
+        Rectangle rect = new Rectangle(40, 806, 60, 788);
+        RadioCheckField radio;
+        PdfFormField radiofield;
+        for (int page = 0; page < LANGUAGES.length; ) {
+            radio = new RadioCheckField(writer, rect, null, LANGUAGES[page]);
+            radio.setBackgroundColor(new GrayColor(0.8f));
+            radiofield = radio.getRadioField();
+            radiofield.setPlaceInPage(++page);
+            radiogroup.addKid(radiofield);
+        }
+        writer.addAnnotation(radiogroup);
+        // add the content
+        for (int i = 0; i < LANGUAGES.length; i++) {
+            cb.beginText();
+            cb.setFontAndSize(bf, 18);
+            cb.showTextAligned(Element.ALIGN_LEFT, LANGUAGES[i], 70, 790, 0);
+            cb.endText();
+            document.newPage();
+        }
+        // step 5
+        document.close();
+    }
+}
---
+++ src/main/java/sandbox/acroforms/RadioGroupMultiPage2.java
@@ -0,0 +1,86 @@
+/*
+ * This example was written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/30895930/issue-with-itext-radiocheckfield-when-displayed-on-multiple-pages
+ */
+package sandbox.acroforms;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfFormField;
+import com.itextpdf.text.pdf.PdfIndirectObject;
+import com.itextpdf.text.pdf.PdfIndirectReference;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPCellEvent;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.RadioCheckField;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Bruno Lowagie (iText Software)
+ */
+public class RadioGroupMultiPage2 {
+    
+    public static final String DEST = "results/acroforms/multipage_radiobutton2.pdf";
+    public static void main(String[] args) throws DocumentException, IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new RadioGroupMultiPage2().createPdf(DEST);
+    }
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+        document.open();
+        PdfFormField radiogroup = PdfFormField.createRadioButton(writer, true);
+        radiogroup.setFieldName("answer");
+        PdfPTable table = new PdfPTable(2);
+        PdfPCell cell;
+        for (int i = 0; i < 25; i++) {
+            cell = new PdfPCell(new Phrase("Question " + i));
+            table.addCell(cell);
+            cell = new PdfPCell(new Phrase("Answer " + i));
+            table.addCell(cell);
+        }
+        for (int i = 0; i <25; i++) {
+            cell = new PdfPCell(new Phrase("Radio: " + i));
+            table.addCell(cell);
+            cell = new PdfPCell();
+            cell.setCellEvent(new MyCellField(writer, radiogroup, "answer" + i));
+            table.addCell(cell);
+        }
+        document.add(table);
+        writer.addAnnotation(radiogroup);
+        document.close();
+    }
+    
+    class MyCellField implements PdfPCellEvent {
+
+        protected PdfWriter writer;
+        protected PdfFormField radiogroup;
+        protected String value;
+        
+        public MyCellField(PdfWriter writer, PdfFormField radiogroup, String value) {
+            this.writer = writer;
+            this.radiogroup = radiogroup;
+            this.value = value;
+        }
+        
+        public void cellLayout(PdfPCell cell, Rectangle position, PdfContentByte[] canvases) {
+            try {
+                RadioCheckField radio = new RadioCheckField(writer, position, null, value);
+                PdfFormField field = radio.getRadioField();
+                writer.addAnnotation(field);
+                radiogroup.addKid(field);
+            } catch (IOException ex) {
+                // do nothing
+            } catch (DocumentException ex) {
+                // do nothing
+            }
+        }
+    }
+}
