f9eda30aa683fd8c68ef0aade9512ca6cdea08bb
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-08-30 16:05:03 +0200

    Updating the comments for a ZUGFeRD example

--- src/main/java/zugferd/XmlInvoicesComfort.java
+++ src/main/java/zugferd/XmlInvoicesComfort.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Example that shows how to create XML invoices.
  */
 package zugferd;
 
