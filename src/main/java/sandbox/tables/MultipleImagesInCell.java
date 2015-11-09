28cfd6d68e019710d8cd8839c6cfe009eb786c60
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-11-09 14:44:38 +0100

    Changing an example into a test

--- src/main/java/sandbox/tables/MultipleImagesInCell.java
+++ src/main/java/sandbox/tables/MultipleImagesInCell.java
@@ -15,10 +15,12 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import sandbox.WrapToTest;
 
 /**
  * @author Bruno Lowagie (iText Software)
  */
+@WrapToTest
 public class MultipleImagesInCell {
     
     public static final String DEST = "results/tables/images_in_cell.pdf";
