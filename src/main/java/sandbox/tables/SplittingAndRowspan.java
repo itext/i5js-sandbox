410ef0602d95d6601bfd606ce660ec307954c3e7
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-02-12 17:12:12 +0100

    New example in answer to question on SO

--- src/main/java/sandbox/tables/SplittingAndRowspan.java
+++ src/main/java/sandbox/tables/SplittingAndRowspan.java
@@ -14,7 +14,9 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import sandbox.WrapToTest;
 
+@WrapToTest
 /**
  * @author Bruno Lowagie (iText Software)
  */
