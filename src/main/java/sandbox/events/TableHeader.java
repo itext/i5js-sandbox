12b1cadba374361539b9ab4637c84c630b7db761
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-02-17 12:10:14 +0100

    removing unnecessary import statements

--- src/main/java/sandbox/events/TableHeader.java
+++ src/main/java/sandbox/events/TableHeader.java
@@ -4,13 +4,10 @@
  */
 package sandbox.events;
 
-import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
