4b625acf99fe1c70f1c8fbb4fd68ec70bc6a23dc
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2016-08-30 11:49:27 +0200

    Changed the JavaOne example

--- src/main/java/javaone/edition16/HelloWorldTable.java
+++ src/main/java/javaone/edition16/HelloWorldTable.java
@@ -8,13 +8,19 @@ package javaone.edition16;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import sandbox.WrapToTest;
 
+@WrapToTest
 public class HelloWorldTable {
     public static final String DEST = "results/javaone/edition16/hello_table.pdf";
 
@@ -28,10 +34,33 @@ public class HelloWorldTable {
         Document document = new Document();
         PdfWriter.getInstance(document, new FileOutputStream(dest));
         document.open();
-        PdfPTable table = new PdfPTable(8);
-        for(int aw = 0; aw < 16; aw++){
-            table.addCell("hi");
-        }
+        PdfPTable table = new PdfPTable(3);
+        PdfPCell cell = new PdfPCell(new Phrase("Cell with colspan 3"));
+        cell.setColspan(3);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase("Cell with rowspan 2"));
+        cell.setRowspan(2);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.addCell(cell);
+        table.addCell("Cell 1.1");
+        cell = new PdfPCell();
+        cell.addElement(new Phrase("Cell 1.2"));
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase("Cell 2.1"));
+        cell.setPadding(5);
+        cell.setUseAscender(true);
+        cell.setUseDescender(true);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(cell);
+        cell = new PdfPCell();
+        cell.setPadding(5);
+        cell.setUseAscender(true);
+        cell.setUseDescender(true);
+        Paragraph p = new Paragraph("Cell 2.2");
+        p.setAlignment(Element.ALIGN_CENTER);
+        cell.addElement(p);
+        table.addCell(cell);
         document.add(table);
         document.close();
     }
